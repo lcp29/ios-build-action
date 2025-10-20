11684df518da1931ad889db7ff5fc5b3b6f625d3
helmholtz <lichangpu29@outlook.com>
2025-10-20 22:50:10 +0800

    Remove unused environment variable assignments
    Removed several environment variable assignments related to Browserstack and other configurations.

--- index.js
+++ index.js
@@ -9,8 +9,6 @@ async function run() {
     ) {
       throw new Error("Browserstack username or access key missing.");
     }
-    process.env.INCREMENT_BUILD_NUMBER = core.getInput("increment-build-number");
-    process.env.TESTFLIGHT_UPLOAD = core.getInput("upload-to-testflight");
     process.env.PROJECT_PATH = core.getInput("project-path");
     process.env.TEAM_ID = core.getInput("team-id");
     process.env.TEAM_NAME = core.getInput("team-name");
@@ -19,18 +17,6 @@ async function run() {
     process.env.CONFIGURATION = core.getInput("configuration");
     process.env.OUTPUT_PATH = core.getInput("output-path");
     process.env.SCHEME = core.getInput("scheme");
-    process.env.BROWSERSTACK_UPLOAD = core.getInput("browserstack-upload");
-    process.env.BROWSERSTACK_USERNAME = core.getInput("browserstack-username");
-    process.env.BROWSERSTACK_ACCESS_KEY = core.getInput("browserstack-access-key");
-    process.env.BUILD_PODS = core.getInput("build-pods");
-    process.env.PODS_PATH = core.getInput("pods-path");
-    process.env.MATCH_PASSWORD = core.getInput("match-password");
-    process.env.MATCH_GIT_URL = core.getInput("match-git-url");
-    process.env.MATCH_GIT_BASIC_AUTHORIZATION = core.getInput("match-git-basic-authorization");
-    process.env.MATCH_BUILD_TYPE = core.getInput("match-build-type");
-    process.env.APPLE_KEY_ID = core.getInput("apple-key-id");
-    process.env.APPLE_KEY_ISSUER_ID = core.getInput("apple-key-issuer-id");
-    process.env.APPLE_KEY_CONTENT = core.getInput("apple-key-content");
     process.env.FASTLANE_VERSION = core.getInput("fastlane-version");
     process.env.FASTLANE_ENV = core.getInput("fastlane-env");
     process.env.IOS_APP_ID = core.getInput("ios-app-id");
